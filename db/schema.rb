d13ab53128d4a4f94ce97a52b58a02ca59795dc5
Rheriel <rheriel@hotmail.co.jp>
2015-01-17 13:59:42 -0600

    Modified the Devise User migration to include only necessary features.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117185616) do
+ActiveRecord::Schema.define(version: 20150117195451) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20150117185616) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",               limit: 255, default: "", null: false
+    t.string   "encrypted_password",  limit: 255, default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
