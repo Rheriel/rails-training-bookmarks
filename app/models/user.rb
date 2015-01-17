fd3e1ba448aaa1fd8c132cd8b9e8a0174015152f
Rheriel <rheriel@hotmail.co.jp>
2015-01-17 14:13:11 -0600

    Created relation between Bookmarks and Users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+
+  has_many :bookmarks
+  
 end
